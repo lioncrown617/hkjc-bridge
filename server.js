458e3ba995062994900d0cba163e4639fb8a651c
lioncrown617 <bevischow2013@gmail.com>
2026-05-25 22:12:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.get('/odds', async (req, res) => {
     });
   }
 });
-const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ HKJC bridge running on port ${PORT}`);
